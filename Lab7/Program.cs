b3467c152bba5b2bdb641e810c50724966a03050
Nastya439 <nastanovikova366@gmail.com>
2024-01-12 20:37:33 +0300

    Добавьте файлы проекта.

---
+++ Lab7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double p = 1;
+
+            for (int a = 1; a < 4; a++)
+            {
+                double s = 0;
+
+                for (int b = 1; b < 11; b++)
+                {
+                    s += Math.Log10(a) + b / 2;
+                }
+                p *= s;
+            }
+
+            Console.WriteLine(p);
+        }
+    }
+}
